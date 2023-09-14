44e1e4b47588cd807ddc0306f201d5424599c538
ranasubham <rsubham@palcnetworks.com>
2023-09-14 13:46:13 +0530

    updated bash and api_fetch

--- api_fetch.py
+++ api_fetch.py
@@ -8,25 +8,17 @@ app = FastAPI(title="SMART Data API")
 async def getInformation(info: Request):
     req_info = await info.json()
     for key, value in req_info.items():
-        smart_5_norm = value.get("smart_5_normalized", -1)
         smart_5_raw = value.get("smart_5_raw", -1)
-        smart_187_norm = value.get("smart_187_normalized", -1)
         smart_187_raw = value.get("smart_187_raw", -1)
-        smart_197_norm = value.get("smart_197_normalized", -1)
         smart_197_raw = value.get("smart_197_raw", -1)
-        smart_198_norm = value.get("smart_198_normalized", -1)
         smart_198_raw = value.get("smart_198_raw", -1)
         data = {
-                "smart_5_normalized" : smart_5_norm,
                 "smart_5_raw" :  smart_5_raw,
-                "smart_187_normalized" : smart_187_norm,
                 "smart_187_raw" :  smart_187_raw,
-                "smart_197_normalized" :  smart_197_norm,
                 "smart_197_raw" :  smart_197_raw,
-                "smart_198_normalized" :  smart_198_norm,
                 "smart_198_raw" :  smart_198_raw
             }
-        #print(data)
+        print(data)
         result_df = predict_failure(data)
 
         # You can return the result or do further processing here
@@ -34,5 +26,4 @@ async def getInformation(info: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main1:app", host="127.0.0.1", port=8000, reload=True)
-
+    uvicorn.run("api_fetch:app", host="127.0.0.1", port=8000, reload=True)
--- bash_send.sh
+++ bash_send.sh
@@ -1,61 +1,52 @@
 #!/bin/bash
 
 function collect_and_send_data {
-  # Collect SMART data and process it with jq
-  partition="/dev/sda"
-  serial_no=$(smartctl -i $partition | grep -oP 'Serial Number:\s+\K\S+' )
-  device_model=$(smartctl -i $partition | grep -oP 'Device Model:\s+\K\S+' )
-  sda_data=$(smartctl -A -j $partition | jq --arg serial "$serial_no" --arg model "$device_model" --arg fallbackValue "null" '
-    .ata_smart_attributes.table
-    | reduce .[] as $item (
-        {};
-        . += {
-          ("serial_no"): $serial,
-          ("device_model"): $model,
-          ("timestamp"): (now | strftime("%Y-%m-%d %H:%M:%S")),
-          ("smart_\($item.id)_normalized"): ($item.value // $fallbackValue),
-          ("smart_\($item.id)_raw"): ($item.raw.value // $fallbackValue)
-        }
-      )
-  ')
-
-  # Collect data from /dev/sdc
+  # Collect SMART data and process it with jq for /dev/sdc
   partition="/dev/sdc"
-  serial_no=$(smartctl -i $partition | grep -oP 'Serial Number:\s+\K\S+'  )
-  device_model=$(smartctl -i $partition | grep -oP 'Device Model:\s+\K\S+'  )
-  sdc_data=$(smartctl -A -j $partition | jq --arg serial "$serial_no" --arg model "$device_model" --arg fallbackValue "null" '
-    .ata_smart_attributes.table
-    | reduce .[] as $item (
-        {};
-        . += {
-          ("serial_no"): $serial,
-          ("device_model"): $model,
-          ("timestamp"): (now | strftime("%Y-%m-%d %H:%M:%S")),
-          ("smart_\($item.id)_normalized"): ($item.value // $fallbackValue),
-          ("smart_\($item.id)_raw"): ($item.raw.value // $fallbackValue)
-        }
-      )
-  ')
 
-  # Combine the data from both partitions into a single JSON object
-  combined_data="{\"sda\": $sda_data, \"sdc\": $sdc_data}"
+  if [ -e "$partition" ]; then
+    serial_no=$(smartctl -i "$partition" | grep -oP 'Serial Number:\s+\K\S+')
+    device_model=$(smartctl -i "$partition" | grep -oP 'Device Model:\s+\K\S+')
+    sdc_data=$(smartctl -A -j "$partition" | jq --arg serial "$serial_no" --arg model "$device_model" --arg fallbackValue "null" '
+      .ata_smart_attributes.table
+      | reduce .[] as $item (
+          {};
+          . += {
+            ("serial_no"): $serial,
+            ("device_model"): $model,
+            ("timestamp"): (now | strftime("%Y-%m-%d %H:%M:%S")),
+            ("smart_\($item.id)_normalized"): ($item.value // $fallbackValue),
+            ("smart_\($item.id)_raw"): ($item.raw.value // $fallbackValue)
+          }
+        )
+    ')
 
-  # Define the FastAPI server URL
-  api_url="http://127.0.0.1:8000/getInformation"
+    # Define the FastAPI server URL
+    api_url="http://127.0.0.1:8000/getInformation"
 
-  # Make a POST request to the server with the JSON payload
-  response=$(curl -s -w "\n%{http_code}" -X POST -H "Content-Type: application/json" -d "$combined_data" "$api_url")
-  http_code=$(echo "$response" | tail -n1)
-  if [ "$http_code" -eq 200 ]; then
-    echo "Data sent successfully"
+    # Make a POST request to the server with the JSON payload
+    response=$(curl -s -w "\n%{http_code}" -X POST -H "Content-Type: application/json" -d "{\"sdc\": $sdc_data}" "$api_url")
+    http_code=$(echo "$response" | tail -n1)
+    if [ "$http_code" -eq 200 ]; then
+      echo "Data sent successfully"
+    else
+      echo "Error: $http_code"
+    fi
   else
-    echo "Error: $http_code"
+    echo "Partition $partition not present. Exiting."
+    exit 1
   fi
 }
 
-while true
-do
-  collect_and_send_data
-  sleep 300
-done
+read -p "Continue with collecting data for /dev/sdc? (yes/no): " user_input
+
+if [ "$user_input" = "yes" ]; then
+  while true
+  do
+    collect_and_send_data
+    sleep 300
+  done
+else
+  echo "Data collection aborted."
+fi
 
